74ef343705cea8e4ff4d3e046ed9fd48802cda34
Kevin <326.osito@gmail.com>
2024-11-23 23:05:51 -0400

    feat: brands CRUD

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,132 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { convertToSlug } from 'src/common/helpers/convert-to-slug';
 
 @Injectable()
 export class BrandsService {
-  create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createBrandDto: CreateBrandDto) {
+    const { name } = createBrandDto;
+    const brandExist = await this.prisma.brands.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (brandExist) {
+      throw new BadRequestException(
+        'Trademark with that name has already been registered',
+      );
+    }
+
+    const slug = convertToSlug(createBrandDto.name);
+    const brand = await this.prisma.brands.create({
+      data: {
+        ...createBrandDto,
+        slug,
+      },
+    });
+
+    return {
+      message: 'Trademark created successfully',
+      brand,
+    };
   }
 
-  findAll() {
-    return `This action returns all brands`;
+  async findAll() {
+    const brands = await this.prisma.brands.findMany();
+
+    return {
+      brands,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  async findOne(term: string) {
+    const brand = await this.prisma.brands.findFirst({
+      where: {
+        OR: [{ id: term }, { slug: term }],
+      },
+    });
+
+    if (!brand) {
+      throw new NotFoundException('Trademark not found');
+    }
+
+    return {
+      brand,
+    };
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  async update(id: string, updateBrandDto: UpdateBrandDto) {
+    const { name } = updateBrandDto;
+    const brandExist = await this.prisma.brands.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!brandExist) {
+      throw new NotFoundException('Trademark not found');
+    }
+
+    if (name) {
+      const slug = convertToSlug(name);
+      const brand = await this.prisma.brands.update({
+        where: {
+          id,
+        },
+        data: {
+          ...updateBrandDto,
+          slug,
+        },
+      });
+
+      return {
+        message: 'Trademark updated',
+        brand,
+      };
+    }
+
+    const brand = await this.prisma.brands.update({
+      where: {
+        id,
+      },
+      data: updateBrandDto,
+    });
+
+    return {
+      message: 'Trademark updated',
+      brand,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  async remove(id: string) {
+    const brandExist = await this.prisma.brands.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!brandExist) {
+      throw new NotFoundException('Trademark not found');
+    }
+
+    const deleteBrand = await this.prisma.brands.delete({
+      where: {
+        id,
+      },
+    });
+
+    return {
+      message: 'Trademark deleted',
+      deleteBrand,
+    };
   }
 }
--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,1 +1,10 @@
-export class CreateBrandDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateBrandDto {
+  @IsString({ message: 'El nombre debe ser un texto' })
+  @IsNotEmpty({ message: 'El nombre de la marca no puede estar vacío.' })
+  name: string;
+  @IsString()
+  @IsOptional()
+  slug: string;
+}
