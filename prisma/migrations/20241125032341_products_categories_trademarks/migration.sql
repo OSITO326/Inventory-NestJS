62990f1cdacfbfb305056efcd98629830d14e53c
Kevin <326.osito@gmail.com>
2024-11-24 23:46:36 -0400

    change: brands to trademarks migration

--- prisma/migrations/20241125032341_products_categories_trademarks/migration.sql
+++ prisma/migrations/20241125032341_products_categories_trademarks/migration.sql
@@ -8,9 +8,9 @@ CREATE TABLE "products" (
     "slug" TEXT NOT NULL,
     "stock" INTEGER NOT NULL,
     "isAvailable" BOOLEAN NOT NULL,
-    "brandsId" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "trademarksId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
 );
@@ -20,29 +20,29 @@ CREATE TABLE "categories" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "brands" (
+CREATE TABLE "trademarks" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "brands_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "trademarks_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "categories_on_products" (
     "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "productsId" TEXT NOT NULL,
     "categoriesId" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "categories_on_products_pkey" PRIMARY KEY ("id")
 );
@@ -54,13 +54,13 @@ CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
 CREATE UNIQUE INDEX "categories_slug_key" ON "categories"("slug");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "brands_name_key" ON "brands"("name");
+CREATE UNIQUE INDEX "trademarks_name_key" ON "trademarks"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "brands_slug_key" ON "brands"("slug");
+CREATE UNIQUE INDEX "trademarks_slug_key" ON "trademarks"("slug");
 
 -- AddForeignKey
-ALTER TABLE "products" ADD CONSTRAINT "products_brandsId_fkey" FOREIGN KEY ("brandsId") REFERENCES "brands"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "products" ADD CONSTRAINT "products_trademarksId_fkey" FOREIGN KEY ("trademarksId") REFERENCES "trademarks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "categories_on_products" ADD CONSTRAINT "categories_on_products_productsId_fkey" FOREIGN KEY ("productsId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
