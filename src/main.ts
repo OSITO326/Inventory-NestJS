dc3de0fa46c922c290f18368e6003dbfb050cfb7
Kevin <326.osito@gmail.com>
2024-11-27 00:16:46 -0400

    add: ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Habilitar transformación
+      whitelist: true, // Ignorar campos no definidos en el DTO
+      forbidNonWhitelisted: true, // Lanzar error si hay campos no permitidos
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
