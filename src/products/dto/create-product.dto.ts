03506abeaf410b863bf7c4b6c908e972b9b8d0ab
Kevin <326.osito@gmail.com>
2024-12-03 21:59:27 -0400

    feat: products crud

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,54 @@
-export class CreateProductDto {}
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsArray,
+  ArrayNotEmpty,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString({ message: 'El nombre debe ser un texto' })
+  @IsNotEmpty({ message: 'El nombre no puede estar vacío.' })
+  name: string;
+
+  @IsString({ message: 'La descripción debe ser un texto' })
+  @IsNotEmpty({ message: 'La descripción no puede estar vacía.' })
+  description: string;
+
+  @IsString({ message: 'La imagen debe ser un texto (URL o base64)' })
+  @IsNotEmpty({ message: 'La imagen no puede estar vacía.' })
+  image: string;
+
+  @IsNumber({}, { message: 'El precio debe ser un número' })
+  @IsNotEmpty({ message: 'El precio no puede estar vacío.' })
+  price: number;
+
+  @IsString({ message: 'El slug debe ser un texto' })
+  @IsOptional()
+  slug: string;
+
+  @IsInt({ message: 'El stock debe ser un número entero' })
+  @IsNotEmpty({ message: 'El stock no puede estar vacío.' })
+  stock: number;
+
+  @IsBoolean({
+    message: 'El estado de disponibilidad debe ser verdadero o falso',
+  })
+  isAvailable: boolean;
+
+  @IsString({ message: 'El sku debe ser un texto' })
+  @IsOptional()
+  sku: string;
+
+  @IsString({ message: 'El ID de la marca debe ser un texto' })
+  @IsNotEmpty({ message: 'El ID de la marca no puede estar vacío.' })
+  trademarksId: string;
+
+  @IsArray({ message: 'Las categorías deben ser un arreglo de IDs' })
+  @ArrayNotEmpty({ message: 'El arreglo de categorías no puede estar vacío.' })
+  @IsString({ each: true, message: 'Cada ID de categoría debe ser un texto' })
+  categories: string[];
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,342 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { convertToSlug } from 'src/common/helpers/convert-to-slug';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const { categories, trademarksId, name, ...productData } = createProductDto;
+
+    // Verify ID of trademark (trademarksId) is valid
+    const validTrademark = await this.prisma.trademarks.findUnique({
+      where: {
+        id: trademarksId,
+      },
+    });
+    if (!validTrademark) {
+      throw new Error('Trademark provided does not exits in the database');
+    }
+
+    // Verify IDs of categories are valid
+    const validCategories = await this.prisma.categories.findMany({
+      where: {
+        id: {
+          in: categories,
+        },
+      },
+    });
+    if (validCategories.length !== categories.length) {
+      throw new Error('Some categories doesnt exist on database');
+    }
+
+    // TODO:
+    // search service image
+    // DO: image??
+
+    // Create product and link trademark and categories
+    const slug = convertToSlug(name);
+    const newProduct = await this.prisma.products.create({
+      data: {
+        name,
+        slug,
+        ...productData,
+        Trademarks: {
+          connect: { id: trademarksId }, // Relation with trademark
+        },
+        Categories: {
+          create: categories.map((categoryId) => ({
+            categoriesId: categoryId,
+          })),
+        },
+      },
+      include: {
+        Categories: {
+          select: {
+            Categories: {
+              select: {
+                id: true,
+                name: true,
+                slug: true,
+              },
+            },
+          },
+        },
+        Trademarks: {
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+          },
+        },
+      },
+    });
+
+    return {
+      message: 'Products created successfully',
+      newProduct,
+    };
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+    const { limit, page, search } = paginationDto;
+    if (!search) {
+      const totalProducts = await this.prisma.products.count();
+      const lastPage = Math.ceil(totalProducts / limit);
+
+      const products = await this.prisma.products.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: {
+          createdAt: 'desc',
+        },
+        include: {
+          Categories: {
+            select: {
+              Categories: {
+                select: {
+                  id: true,
+                  name: true,
+                  slug: true,
+                },
+              },
+            },
+          },
+          Trademarks: {
+            select: {
+              id: true,
+              name: true,
+              slug: true,
+            },
+          },
+        },
+      });
+
+      return {
+        products,
+        meta: {
+          total: totalProducts,
+          page,
+          lastPage,
+        },
+      };
+    }
+
+    const totalProducts = await this.prisma.products.count({
+      where: {
+        OR: [
+          { id: { contains: search } },
+          { name: { contains: search } },
+          { sku: { contains: search } },
+        ],
+      },
+    });
+    const lastPage = Math.ceil(totalProducts / limit);
+    const products = await this.prisma.products.findMany({
+      where: {
+        OR: [
+          { id: { contains: search } },
+          { name: { contains: search } },
+          { sku: { contains: search } },
+        ],
+      },
+      skip: (page - 1) * limit,
+      take: limit,
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        Categories: {
+          select: {
+            categoriesId: true,
+            Categories: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+        Trademarks: {
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+          },
+        },
+      },
+    });
+
+    return {
+      products,
+      meta: {
+        total: totalProducts,
+        page,
+        lastPage,
+      },
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(term: string) {
+    try {
+      const product = await this.prisma.products.findFirst({
+        where: {
+          OR: [{ id: term }, { slug: term }],
+        },
+      });
+      if (!product) {
+        throw new BadRequestException('Product not found');
+      }
+
+      return {
+        product,
+      };
+    } catch (error) {
+      throw new BadRequestException({
+        message: 'An error occurred',
+        error,
+        statusCode: HttpStatus.NOT_FOUND,
+      });
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const { categories, trademarksId, name, ...productData } = updateProductDto;
+
+    // Verify if product exist
+    const productExist = await this.prisma.products.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!productExist) {
+      throw new BadRequestException(`Product with ID ${id} not found`);
+    }
+
+    // Verify if trademarksId are valid
+    if (trademarksId) {
+      const validTrademark = await this.prisma.trademarks.findUnique({
+        where: {
+          id: trademarksId,
+        },
+      });
+
+      if (!validTrademark) {
+        throw new BadRequestException('Invalid trademark ID provided');
+      }
+    }
+
+    // Verify if categories are valid
+    if (categories && categories.length > 0) {
+      const validCategories = await this.prisma.categories.findMany({
+        where: {
+          id: { in: categories },
+        },
+      });
+
+      if (validCategories.length !== categories.length) {
+        throw new BadRequestException(
+          'Some categories do not exist in the database',
+        );
+      }
+    }
+
+    // Generate slug if provide new name
+    const slug = name ? convertToSlug(name) : undefined;
+
+    // Update product
+    const updateProduct = await this.prisma.products.update({
+      where: {
+        id,
+      },
+      data: {
+        ...productData,
+        ...(name && { name, slug }),
+        ...(trademarksId && {
+          Trademarks: {
+            connect: {
+              id: trademarksId, // connect with new trademark
+            },
+          },
+        }),
+        ...(categories && {
+          Categories: {
+            deleteMany: {}, // Delete actual relationships
+            create: categories.map((categoryId) => ({
+              categoriesId: categoryId,
+            })), // Create new relationships
+          },
+        }),
+      },
+      include: {
+        Categories: {
+          select: {
+            Categories: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+        Trademarks: {
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+          },
+        },
+      },
+    });
+
+    return {
+      message: 'Product updated successfully',
+      updateProduct,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    // Verify if product exists
+    const productExist = await this.prisma.products.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!productExist) {
+      throw new BadRequestException(`Product with ID ${id} not found`);
+    }
+
+    // Delete product and relationships
+    const deleteProduct = await this.prisma.products.delete({
+      where: {
+        id,
+      },
+    });
+    // this block if schema.prisma categoriesOnProducts not set delete on Cascade
+    /* const deleteProduct = await this.prisma.$transaction([
+      // delete relationships
+      this.prisma.categoriesOnProducts.deleteMany({
+        where: {
+          productsId: id,
+        },
+      }),
+      // delete product
+      this.prisma.products.delete({
+        where: {
+          id,
+        },
+      }),
+    ]); */
+
+    return {
+      message: `Product wiht ID ${id} has been removed successfully`,
+      deleteProduct,
+    };
   }
 }
